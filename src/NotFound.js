a94d56362cca6bcf910253085bc362cd21ea89f8
Hulk <hulk@yahoo.com>
2021-05-19 13:59:34 +0430

    main

---
+++ src/NotFound.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom"
+
+const NotFound = () => {
+    return ( 
+        <div className="not_found">
+            <h2>Sorry</h2>
+            <p>That page cannot be found !</p>
+
+            <Link to="/"> Back </Link>
+
+            
+        </div>
+     );
+}
+ 
+export default NotFound;
